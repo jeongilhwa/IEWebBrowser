ad6bf8e2e89cf476ee73fd61650250b3be3f0aa3
jeongilhwa <jeongilhwa93@gmail.com>
2021-04-16 20:19:43 +0900

    초기 커밋.

---
+++ IEWebBrowser/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace IEWebBrowser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.BackButton = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.WebAddressTextBox = new System.Windows.Forms.TextBox();
+            this.AddressMoveButton = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.webBrowser1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 28F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(3, 31);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(794, 416);
+            this.webBrowser1.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.AddressMoveButton);
+            this.panel1.Controls.Add(this.WebAddressTextBox);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.BackButton);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 28);
+            this.panel1.TabIndex = 1;
+            // 
+            // BackButton
+            // 
+            this.BackButton.Location = new System.Drawing.Point(4, 2);
+            this.BackButton.Name = "BackButton";
+            this.BackButton.Size = new System.Drawing.Size(75, 23);
+            this.BackButton.TabIndex = 0;
+            this.BackButton.Text = "뒤로";
+            this.BackButton.UseVisualStyleBackColor = true;
+            this.BackButton.Click += new System.EventHandler(this.BackButton_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(85, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "앞으로";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // WebAddressTextBox
+            // 
+            this.WebAddressTextBox.Location = new System.Drawing.Point(166, 0);
+            this.WebAddressTextBox.Name = "WebAddressTextBox";
+            this.WebAddressTextBox.Size = new System.Drawing.Size(531, 25);
+            this.WebAddressTextBox.TabIndex = 2;
+            this.WebAddressTextBox.TextChanged += new System.EventHandler(this.WebAddressTextBox_TextChanged);
+            // 
+            // AddressMoveButton
+            // 
+            this.AddressMoveButton.Location = new System.Drawing.Point(722, 3);
+            this.AddressMoveButton.Name = "AddressMoveButton";
+            this.AddressMoveButton.Size = new System.Drawing.Size(75, 23);
+            this.AddressMoveButton.TabIndex = 3;
+            this.AddressMoveButton.Text = "이동";
+            this.AddressMoveButton.UseVisualStyleBackColor = true;
+            this.AddressMoveButton.Click += new System.EventHandler(this.AddressMoveButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button AddressMoveButton;
+        private System.Windows.Forms.TextBox WebAddressTextBox;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button BackButton;
+    }
+}
+
---
+++ IEWebBrowser/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IEWebBrowser
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BackButton_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoBack();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoForward();
+        }
+
+        private void WebAddressTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AddressMoveButton_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate(WebAddressTextBox.Text);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            webBrowser1.Navigate("https://accounts.kakao.com/login?continue=https%3A%2F%2Flogins.daum.net%2Faccounts%2Fksso.do%3Frescue%3Dtrue%26url%3Dhttps%253A%252F%252Fwww.daum.net%252F");
+            WebLogin();
+        }
+
+        private async void WebLogin()
+        {
+            await Task.Delay(1500);
+            webBrowser1.Document.GetElementById("id_email_2").SetAttribute("value", "ghqnftk@kakao.com");
+            webBrowser1.Document.GetElementById("id_password_3").SetAttribute("value", "wjddlfghk1");
+           
+            await Task.Delay(1500);
+            HtmlElementCollection loginButton = webBrowser1.Document.GetElementsByTagName("button");
+            foreach (HtmlElement seachLogin in loginButton)
+            {
+                if (seachLogin.InnerText == "로그인")
+                {
+                    seachLogin.InvokeMember("click");
+                }
+            }
+
+        }
+    }
+}
